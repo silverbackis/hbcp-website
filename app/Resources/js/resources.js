35f14a809c50af43bbf872c23a77163f51101863
Daniel West <daniel@silverback.is>
2018-06-01 15:11:56 +0100

    Diagram Interactive + Team Tabs

--- app/Resources/js/resources.js
+++ app/Resources/js/resources.js
@@ -30,4 +30,50 @@ $(function(){
     });
     $filterCount.html(count);
   })
+
+  const $categorySelectPicker = $('#category-select');
+  const $categorySelectPickerOptions = $('option', $categorySelectPicker);
+  const $bsLink = $('.bs-diagram-link');
+  $bsLink.on('click', function(e) {
+    e.preventDefault();
+    const category = $(this).attr('xlink:href').replace(/^#/, '');
+    const strLen = category.length
+    let values = []
+    $categorySelectPickerOptions.each(function(i, op) {
+      const opText = op.innerHTML.toLowerCase();
+      if (opText.substr(0, strLen) === category) {
+        values.push(op.value)
+      }
+    });
+    $categorySelectPicker.selectpicker('val', values);
+  });
+
+
+  let categories = [];
+  $bsLink.each(function(i, link) {
+    categories.push(link.getAttributeNS('http://www.w3.org/1999/xlink', 'href').replace(/^#/, ''))
+  });
+  $categorySelectPicker.on('changed.bs.select', function() {
+    $bsLink.removeClass('active');
+    const newVals = $categorySelectPicker.selectpicker('val');
+    let selectedFirstWord = null;
+    let allSelectedSameFirstWord = true;
+    $categorySelectPickerOptions.each(function(i, op) {
+      if(newVals.indexOf(op.value) !== -1) {
+        let firstWord = op.innerHTML.toLowerCase().split(' ')[0];
+        if (!selectedFirstWord) {
+          selectedFirstWord = firstWord
+        } else if(selectedFirstWord !== firstWord) {
+          allSelectedSameFirstWord = false;
+          return false;
+        }
+      }
+    });
+    if (allSelectedSameFirstWord) {
+      const $activeBsLink = $bsLink.filter('[xlink\\:href="#'+selectedFirstWord+'"]');
+      if ($activeBsLink.length === 1) {
+        $activeBsLink.addClass('active');
+      }
+    }
+  })
 });
